0275f39747796e8c597e756b071439b81190e5ac
juckphai <llsvuslnw@gmail.com>
2025-07-12 15:13:50 +0700

    Add files via upload

---
+++ sw-ลาวสตาร์.js.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'juckphai-lao-star-app-v1';
+const urlsToCache = [
+  './',
+  './10.html',
+  './01.html',  // หน้าเมนูหลัก
+  './logo.png',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest-ลาวสตาร์.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache for Lao Star App');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
